ea8f361ac68b0e168d9f57fd86d71e7bcfc6928f
ParkerIndustries <mickael.k.swaminathan@gmail.com>
2021-03-09 18:50:46 +0100

    Message input done

--- src/components/ChatInput.js
+++ src/components/ChatInput.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import styled from 'styled-components'
-
+import Send from '@material-ui/icons/Send'
 function ChatInput() {
     return (
         <Container>
             <InputContainer>
                 <form>
                     <input type="text" placeholder="Type your message..." />
+                    <SendButton>
+                        <Send />
+                    </SendButton>
                 </form>
             </InputContainer>
         </Container>
@@ -16,8 +19,49 @@ function ChatInput() {
 export default ChatInput;
 
 const Container = styled.div`
-
+    padding-left: 20px;
+    padding-right: 20px;
+    padding-bottom: 24px;
 `
 const InputContainer = styled.div`
+    border: 1px solid #8D8D8E;
+    border-radius: 4px;
+    
+    form {
+        display: flex;
+        height: 42px;
+        align-items: center;
+        padding-left: 10px;
 
+        input {
+            flex: 1;
+            border: none;
+            font-size: 13px;
+        }
+
+        input:focus {
+            outline: none;
+        }
+    }
+`
+
+const SendButton = styled.div`
+    background: #606060;
+    color: #d9d9d9;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 32px;
+    height: 32px;
+    border-radius: 4px;
+    margin-right: 5px;
+    cursor: pointer;
+
+    .MuiSvgIcon-root {
+        width: 18px;
+    }
+
+    :hover {
+        background: #007a5a;
+    }
 `

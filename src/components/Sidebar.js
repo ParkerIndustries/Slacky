3d85763aa6cc075f12f2bba137ab822fcce48469
ParkerIndustries <mickael.k.swaminathan@gmail.com>
2021-03-09 02:36:25 +0100

    Header pushed

---
+++ src/components/Sidebar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Sidebar() {
+    return (
+        <div style={{ background: "darkslategrey"}}>
+            Sidebar
+        </div>
+    )
+}
+
+export default Sidebar
